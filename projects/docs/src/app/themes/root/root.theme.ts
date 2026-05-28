a9aae2228f6f887cb42beae3a97a6860dd12d486
Christian Ezeani <christian.ezeani@gmail.com>
2026-05-28 15:19:14 +0100

    WIP: Documentation

--- projects/docs/src/app/themes/root/root.theme.ts
+++ projects/docs/src/app/themes/root/root.theme.ts
@@ -1,13 +1,16 @@
 import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'root-theme',
   templateUrl: 'root.theme.html',
   styleUrls: ['root.theme.scss'],
-  imports: [],
+  imports: [
+    RouterLink,
+  ],
 })
 export class RootTheme {
 
-  //
+  protected readonly year = 2026;
 
 }
