9c36ce1c7ec5a7b0e5440ccb814ce718b8053917
Christian Ezeani <christian.ezeani@gmail.com>
2026-08-10 12:46:32 +0100

    Add SerialGuard Features

--- projects/ngflux/src/lib/guards/SerialGuard.ts
+++ projects/ngflux/src/lib/guards/SerialGuard.ts
@@ -16,25 +16,29 @@ import { from, Observable, of, switchMap } from "rxjs";
 
 export namespace NgFlux.SerialGuard {
 
+  function run<T extends Function>(guards: T[], callback: (guard: T) => MaybeAsync<GuardResult>): MaybeAsync<GuardResult> {
+    let result: MaybeAsync<GuardResult> = of(true);
+
+    for (let guard of guards) {
+      result = result.pipe(
+        switchMap(resp => {
+          if (resp !== true) return of(resp);
+
+          const output = callback(guard);
+          if (output instanceof Observable) return output;
+          if (output instanceof Promise) return from(output);
+
+          return of(output);
+        })
+      );
+    }
+
+    return result;
+  }
+
   export function canActivateFn(...guards: CanActivateFn[]): CanActivateFn {
     return (route: ActivatedRouteSnapshot, state: RouterStateSnapshot): MaybeAsync<GuardResult> => {
-      let result: MaybeAsync<GuardResult> = of(true);
-
-      for (let guard of guards) {
-        result = result.pipe(
-          switchMap(resp => {
-            if (resp !== true) return of(resp);
-
-            const output = guard(route, state);
-            if (output instanceof Observable) return output;
-            if (output instanceof Promise) return from(output);
-
-            return of(output);
-          })
-        );
-      }
-
-      return result;
+      return run(guards, guard => guard(route, state));
     };
   }
 
@@ -44,45 +48,13 @@ export namespace NgFlux.SerialGuard {
 
   export function canDeactivateFn<T>(...guards: CanDeactivateFn<T>[]): CanDeactivateFn<T> {
     return (component: T, currentRoute: ActivatedRouteSnapshot, currentState: RouterStateSnapshot, nextState: RouterStateSnapshot): MaybeAsync<GuardResult> => {
-      let result: MaybeAsync<GuardResult> = of(true);
-
-      for (let guard of guards) {
-        result = result.pipe(
-          switchMap(resp => {
-            if (resp !== true) return of(resp);
-
-            const output = guard(component, currentRoute, currentState, nextState);
-            if (output instanceof Observable) return output;
-            if (output instanceof Promise) return from(output);
-
-            return of(output);
-          })
-        );
-      }
-
-      return result;
+      return run(guards, guard => guard(component, currentRoute, currentState, nextState));
     };
   }
 
   export function canMatchFn(...guards: CanMatchFn[]): CanMatchFn {
     return (route: Route, segments: UrlSegment[], currentSnapshot: PartialMatchRouteSnapshot): MaybeAsync<GuardResult> => {
-      let result: MaybeAsync<GuardResult> = of(true);
-
-      for (let guard of guards) {
-        result = result.pipe(
-          switchMap(resp => {
-            if (resp !== true) return of(resp);
-
-            const output = guard(route, segments, currentSnapshot);
-            if (output instanceof Observable) return output;
-            if (output instanceof Promise) return from(output);
-
-            return of(output);
-          })
-        );
-      }
-
-      return result;
+      return run(guards, guard => guard(route, segments, currentSnapshot));
     }
   }
 
