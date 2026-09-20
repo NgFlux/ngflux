8f674c662db4bc2d0c7e3a3095f6ba928a891abf
Christian Ezeani <christian.ezeani@gmail.com>
2026-09-20 13:56:04 +0100

    Add [flat] to NgFluxDialogBody

--- projects/ngflux/src/lib/components/dialog/body/body.component.ts
+++ projects/ngflux/src/lib/components/dialog/body/body.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { booleanAttribute, Component, input } from '@angular/core';
 
 @Component({
   selector: 'ngf-dialog-body',
   templateUrl: 'body.component.html',
   styleUrls: ['body.component.scss'],
   imports: [],
+  host: {
+    '[class.flat]': 'flat()',
+  },
 })
 export class NgFluxDialogBody {
-  //
+
+  readonly flat = input(false, { transform: booleanAttribute });
+
 }
